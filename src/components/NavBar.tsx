6c8b5c3b5cf82bfb0d66e11a225b95cd4f9ec170
Song Yang <keesongyang4@gmail.com>
2025-07-19 02:23:30 +0800

    feat: enhance navigation styling and functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,6 @@
 import Link from "next/link";
 import { useState, useRef, useEffect } from "react";
 
-const hyperlinkStyle =
-  "text-white font-bold text-[0.85em] hover:text-khaki hover:font-bold mx-4 tracking-[0.2em] transition-all transform ease-linear";
-
 const NavBar: React.FC = () => {
   const [toggleNav, setToggleNav] = useState(false);
   const navRef = useRef<HTMLDivElement>(null);
@@ -25,30 +22,30 @@ const NavBar: React.FC = () => {
 
   return (
     <>
-      <div className="fixed top-5 bg-black bg-opacity-70  px-6 py-2 rounded-3xl hidden md:block z-50">
-        <Link href="#hero" className={hyperlinkStyle}>
+      <div className="fixed top-5 bg-black bg-opacity-70 px-6 py-2 rounded-3xl hidden md:block z-50">
+        <a href="#hero" className="nav-link">
           Home
-        </Link>
-        <Link href="#experience" className={hyperlinkStyle}>
+        </a>
+        <Link href="#experience" className="nav-link">
           Experience
         </Link>
-        <Link href="#projects" className={hyperlinkStyle}>
+        <Link href="#projects" className="nav-link">
           Projects
         </Link>
-        <Link href="#contact" className={hyperlinkStyle}>
+        <Link href="#contact" className="nav-link">
           Contact
         </Link>
         <Link
           href="/keesongyang_resume.pdf"
           target="_blank"
-          className={hyperlinkStyle}
+          className="nav-link"
         >
           Resume
         </Link>
       </div>
 
       <div
-        className="fixed top-5 left-0 px-6 py-2 rounded-3xl block md:hidden z-50"
+        className="fixed top-5 right-0 px-6 py-2 rounded-3xl block md:hidden z-50"
         style={{ paddingTop: "env(safe-area-inset-top)" }}
         ref={navRef}
       >
@@ -70,31 +67,31 @@ const NavBar: React.FC = () => {
           </svg>
         </button>
         {toggleNav ? (
-          <div className="flex flex-col bg-black bg-opacity-80 border-[1px] border-white rounded-xl -ml-1 mt-1">
+          <div className="fixed top-15 right-3 flex flex-col bg-black bg-opacity-80 border-[1px] border-white rounded-xl -ml-1 mt-1">
             <Link
               href="#"
-              className={`${hyperlinkStyle} p-2 pt-4 border-b-[0.5px] border-white`}
+              className={`nav-link p-2 pt-4 border-b-[0.5px] border-white`}
               onClick={() => setToggleNav(false)}
             >
               Home
             </Link>
             <Link
               href="#experience"
-              className={`${hyperlinkStyle} p-2 border-b-[0.5px] border-white`}
+              className={`nav-link p-2 border-b-[0.5px] border-white`}
               onClick={() => setToggleNav(false)}
             >
               Experience
             </Link>
             <Link
               href="#projects"
-              className={`${hyperlinkStyle} p-2 border-b-[0.5px] border-white`}
+              className={`nav-link p-2 border-b-[0.5px] border-white`}
               onClick={() => setToggleNav(false)}
             >
               Projects
             </Link>
             <Link
               href="#contact"
-              className={`${hyperlinkStyle} p-2 border-b-[0.5px] border-white`}
+              className={`nav-link p-2 border-b-[0.5px] border-white`}
               onClick={() => setToggleNav(false)}
             >
               Contact
@@ -102,7 +99,7 @@ const NavBar: React.FC = () => {
             <Link
               href="/KeeSongYang_Resume.pdf"
               target="_blank"
-              className={`${hyperlinkStyle} p-2 pb-4`}
+              className={`nav-link p-2 pb-4`}
               onClick={() => setToggleNav(false)}
             >
               Resume
